f74ab37fa44e998cd0d866a470b22efee0a6e78b
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-06-19 16:34:29 -0400

    made some scriptable objects and a prototyp for the player

---
+++ Survival Checkers/Assets/Scripts/Entities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Entity", menuName ="Entity")]
+public class Entities : ScriptableObject
+{
+    public new string name;
+    
+    public int health;
+    
+    public Sprite artwork;
+
+    public Collision2D collider;
+
+    public void Print()
+    {
+        Debug.Log("Health: " + health
+            +"\nArtwork: "+artwork);
+        
+    }
+
+    public void AffectHealth(int changeHealthBy)
+    {
+        if(health > 0 )
+        {
+            health += changeHealthBy;
+            Debug.Log(health);
+            
+        }
+        else
+        {
+            Debug.Log("dead");
+        }
+        
+    }
+}
--- Survival Checkers/Assets/Scripts/Player.cs
+++ Survival Checkers/Assets/Scripts/Player.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class things : MonoBehaviour
+public class Player : MonoBehaviour
 {
+
+    public Entities player;
+    public Image Artwork;
+    public Text health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Artwork.sprite = player.artwork;
+        health.text = player.health.ToString();
+       
     }
 
     // Update is called once per frame
